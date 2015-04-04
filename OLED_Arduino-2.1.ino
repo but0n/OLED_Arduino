db36651b9919951da021d502384b2692bc379740
but0n <32700567@qq.com>
2015-04-04 05:42:29 +0000

    Update OLED_Arduino-2.1.ino

--- OLED_Arduino-2.1.ino
+++ OLED_Arduino-2.1.ino
@@ -21,26 +21,15 @@
 void OLED_INIT ();
 void w_cmd (uint8_t c);
 void w_data (uint8_t d);
-void refresh_GRAM ();
-void draw_point (uint8_t x, uint8_t y, uint8_t t);
-void draw_lineX (uint8_t x1, uint8_t y1, uint8_t L);
 void clear_display ();
-void DISPLAY1 (uint8_t page, uint8_t start);
-void oprint(uint8_t da, uint8_t page, uint8_t start);
-uint8_t OLED_GRAM[8][128];
-uint8_t number_1[2][16] = {
-0x00, 0x00, 0x00, 0x00, 0x00, 0x04, 0x04, 0x02, 
-0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
-0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
-0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-};
+void oprint(uint8_t word, uint8_t page, uint8_t start);
+
 uint8_t a[2][8] = {
 0x00, 0xA0, 0xA0, 0xA0, 0xE0, 0XC0, 0X00, 0x00, 
 0x07, 0x0F, 0x08, 0x08, 0x07, 0X0F, 0x08, 0x00
 };
 
-//0x00, 0xE0, 0x05, 0xF0, 0x05, 0x10, 0x05, 0x10, 
-//0x07, 0xE0, 0X03, 0XF0, 0X00, 0x10, 0x00, 0x00
+
     void setup (){
       
       OLED_INIT ();
@@ -49,64 +38,7 @@ uint8_t a[2][8] = {
 
     void loop (){
 
-//w_cmd (0x81); // 9.Set Contrast Control   0x66
-//                  w_cmd (0x00); // 0xCF
-//      uint8_t light;
-//      for (light = 0; light < 0x66; light++){
-//                  w_cmd (0x81); // 9.Set Contrast Control   0x66
-//                  w_cmd (light); // 0xCF
-//                  delay (100);
-//      }           
-//      delay (1500);
-      //        Display Logo
 
-                    clear_display ();
-                    DISPLAY1 (0, 58);
-                    w_cmd (0xAF);
-                    delay (1500);
-                    //
-                    w_cmd (0xAE);
-                    delay (1000);
-                    clear_display ();
-                    DISPLAY1 (0, 0);
-                    DISPLAY1 (2, 16);
-                    DISPLAY1 (4, 32);
-                    DISPLAY1 (6, 48);
-                    DISPLAY1 (0, 112);
-                    DISPLAY1 (2, 96);
-                    DISPLAY1 (4, 80);
-                    DISPLAY1 (6, 64);
-                    w_cmd (0xAF);
-                    delay (1500);
-                    
-                    w_cmd (0xAE);
-                    delay (1000);
-                    clear_display ();
-                    oprint ('a', 2, 56);
-                    w_cmd (0xAF);
-                    delay (1500);
-                    
-                    w_cmd (0xAE);
-                    delay (1000);
-                    clear_display ();
-                    oprint ('a', 0, 56);
-                    w_cmd (0xAF);
-                    delay (1500);
-                    
-                    w_cmd (0xAE);
-                    delay (1000);
-                    clear_display ();
-                    oprint ('a', 6, 0);
-                    oprint ('a', 6, 8);
-                    oprint ('a', 6, 16);
-                    oprint ('a', 6, 24);
-                    oprint ('a', 6, 32);
-                    oprint ('a', 6, 40);
-                    
-                    w_cmd (0xAF);
-                    delay (1000);
-                                         
-      
     }
 
           void OLED_INIT (){
@@ -198,7 +130,7 @@ uint8_t a[2][8] = {
                 digitalWrite (CS, HIGH);
               }
                     
-                    void refresh_GRAM (){
+                    void clear_display (){
                      
                             uint8_t x, y; // x = SEG  [0:127]    y = PAGE  [0:7]
                             for (y = 0; y < 8; y++){
@@ -206,29 +138,15 @@ uint8_t a[2][8] = {
                                     w_cmd (0x00);  //  Set Low  (4bit)
                                     w_cmd (0x10);  //  Set High (4bit)
                                     for (x = 0; x<128; x++)
-                                            w_data (OLED_GRAM[y][x]);
+                                            w_data (0x00);
                             
                             }
                     }
-                    void clear_display (){
-                      memset (OLED_GRAM, 0x00, sizeof (OLED_GRAM));
-                      refresh_GRAM ();
-                    }
                     
-                    void DISPLAY1 (uint8_t page, uint8_t start){
-                                    uint8_t t, n;
-                                    for (t = 0; t < 2; t++){
-                                          w_cmd (0xB0 + page + t);
-                                          w_cmd (start & 0x0F);
-                                          w_cmd (((start >> 4) & 0x0F) | 0x10);
-                                          for (n = 0; n < 16; n++){
-                                          w_data (number_1[t][n]);}
-                                    
-                                    }
                     
-                    }
                     
-    void oprint(uint8_t da, uint8_t page, uint8_t start){
+                    
+    void oprint(uint8_t word, uint8_t page, uint8_t start){
       
                 uint8_t t, n;
                 for (t = 0; t < 2; t++){
